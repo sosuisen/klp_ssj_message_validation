cff8153f203c89a102ba683756986068f1799bb2
sosuisen <hidekazu.kubota@gmail.com>
2023-07-20 19:35:22 +0900

    fix

--- src/main/java/com/example/model/validator/WordValidator.java
+++ src/main/java/com/example/model/validator/WordValidator.java
@@ -4,10 +4,12 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class WordValidator implements ConstraintValidator<ValidWord, String> {
-	String[] ngWord = { "りんご", "バナナ", "すいか" };
+	private final String[] ngWord = { "りんご", "バナナ", "すいか" };
 
 	@Override
     public boolean isValid(String message, ConstraintValidatorContext context) {
+		if (message == null) return true;
+
 		// messageがngWordをどれか１つでも含んでいたらfalseを返す
 		for (String word : ngWord) {
 			if (message.contains(word)) return false;
